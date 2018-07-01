4a226a8872b1fbe5b190994cfdbf3f96ba6c0040
Nick Stein <nick@nickstein.net>
2018-07-01 14:18:41 -0400

    Add project files.

---
+++ google-photos-downloader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace google_photos_downloader {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
